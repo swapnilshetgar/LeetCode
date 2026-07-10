ce4095b6fefcd4f6bf984fd8ff2c544a0e780d6c
sapiproxy <pureeswapnil@gmail.com>
2026-07-11 00:05:13 +0530

    Longest Common Prefix file adding

---
+++ Brute-Force/Arrays/Longest_Common_Prefix.py
@@ -0,0 +1,14 @@
+def longestCommonPrefix(strs):
+    if not strs:
+        return ""
+
+    prefix = strs[0]
+
+    for word in strs[1:]:
+        while not word.startswith(prefix):
+            prefix = prefix[:-1]
+
+            if prefix == "":
+                return ""
+
+    return prefix

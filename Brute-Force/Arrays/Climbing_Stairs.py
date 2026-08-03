f179d9034ed3a0c378c4b3acfdb9cbb9929269d0
Swapnil Shetgar <shetgarswapnil@gmail.com>
2026-08-03 21:21:11 +0530

    Climbing Stairs file adding

---
+++ Brute-Force/Arrays/Climbing_Stairs.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 2:
+            return n
+        
+        a = 1
+        b = 2
+        
+        for i in range(3, n + 1):
+            c = a + b
+            a = b
+            b = c
+        
+        return b

8e5dad6578548fc8e5ab2971d8425ac461db276c
swapnilshetgar <shetgarswapnil@gmail.com>
2026-08-26 22:46:36 +0530

    Reverse Integer file adding

---
+++ Brute-Force/Arrays/Reverse_Integer.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        INT_MIN, INT_MAX = -2**31, 2**31 - 1
+        res = 0
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+
+        while x != 0:
+            digit = x % 10
+            x //= 10
+
+            # Check overflow before adding digit
+            if res > (INT_MAX - digit) // 10:
+                return 0
+
+            res = res * 10 + digit
+
+        return sign * res

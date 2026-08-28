34ad1670a56bc4445a8b55e01c97d877732b094f
swapnilshetgar <shetgarswapnil@gmail.com>
2026-08-28 12:31:40 +0530

    Two Sum II - Input Array Is Sorted file adding

---
+++ Brute-Force/Arrays/Two_Sum_II_Input_Array_Is_Sorted.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        left=0
+        right=len(numbers)-1
+        for i in range(len(numbers)):
+            rem=numbers[left]+numbers[right]
+
+            if rem==target:
+                return [left+1,right+1]
+
+            if rem >target:
+                right-=1
+
+            if rem<target:
+                left+=1

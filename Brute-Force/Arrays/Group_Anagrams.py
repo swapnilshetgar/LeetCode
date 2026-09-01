abf0be1f35c4149fb3fe6452900437e2853bfa32
swapnilshetgar <shetgarswapnil@gmail.com>
2026-09-01 22:06:31 +0530

    Group Anagrams file adding

---
+++ Brute-Force/Arrays/Group_Anagrams.py
@@ -0,0 +1,23 @@
+class Solution(object):
+
+    def sortString(self, s):
+        s = list(s)
+        s.sort()
+        return "".join(s)
+
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        dict1 = {}
+
+        for s in strs:
+            key = self.sortString(s)
+
+            if key in dict1:
+                dict1[key].append(s)
+            else:
+                dict1[key] = [s]
+
+        return list(dict1.values())

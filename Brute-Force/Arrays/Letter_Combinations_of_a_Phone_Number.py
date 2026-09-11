a4f0d8243d53a6828d0f38d100485b35284209f0
swapnilshetgar <shetgarswapnil@gmail.com>
2026-09-11 23:18:53 +0530

    Letter_Combinations_of_a_Phone_Number.py file adding

---
+++ Brute-Force/Arrays/Letter_Combinations_of_a_Phone_Number.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+
+        phone = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+
+        result = []
+
+        def backtrack(index, current):
+            if index == len(digits):
+                result.append(current)
+                return
+
+            for char in phone[digits[index]]:
+                backtrack(index + 1, current + char)
+
+        backtrack(0, "")
+        return result

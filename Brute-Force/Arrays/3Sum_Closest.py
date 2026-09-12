8932805b15412f3ce991aa23ad45fa493aa4c5b4
swapnilshetgar <shetgarswapnil@gmail.com>
2026-09-12 21:24:54 +0530

    3Sum Closest file adding

---
+++ Brute-Force/Arrays/3Sum_Closest.py
@@ -0,0 +1,28 @@
+class Solution:
+    def threeSumClosest(self, nums, target):
+        nums.sort()
+
+        closest = nums[0] + nums[1] + nums[2]
+
+        for i in range(len(nums) - 2):
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                # Exact match
+                if total == target:
+                    return total
+
+                # Update closest sum
+                if abs(total - target) < abs(closest - target):
+                    closest = total
+
+                # Move pointers
+                if total < target:
+                    left += 1
+                else:
+                    right -= 1
+
+        return closest

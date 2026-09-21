883f6c2f36a91c925244a2accb653494f7739857
swapnilshetgar <shetgarswapnil@gmail.com>
2026-09-21 22:40:54 +0530

    Range Sum Query - Immutable file adding

---
+++ Brute-Force/Arrays/Range_Sum_Query - Immutable.py
@@ -0,0 +1,22 @@
+class NumArray(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.prefix = [0] * (len(nums) + 1)
+        for i in range(len(nums)):
+            self.prefix[i + 1] = self.prefix[i] + nums[i]
+
+    def sumRange(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: int
+        """
+        return self.prefix[right + 1] - self.prefix[left]
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)

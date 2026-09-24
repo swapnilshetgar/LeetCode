7638a798760d4a9aedc2a0c9a9f1a37602d672f8
swapnilshetgar <shetgarswapnil@gmail.com>
2026-09-24 19:55:36 +0530

    Remove Nth Node From End of List file adding

---
+++ Brute-Force/Arrays/Remove_Nth_Node_From_End_of_List.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: Optional[ListNode]
+        :type n: int
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode(0, head)
+        first = dummy
+        second = dummy
+
+        # Move first pointer n+1 steps ahead
+        for _ in range(n + 1):
+            first = first.next
+
+        # Move both pointers until first reaches the end
+        while first:
+            first = first.next
+            second = second.next
+
+        # Skip the target node
+        second.next = second.next.next
+
+        return dummy.next
